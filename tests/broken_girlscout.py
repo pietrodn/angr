5faac244904c654398426893439d14a23c4a650a
Pietro De Nicolao <pietro@ProPietro.local>
2017-05-06 16:32:18 +0200

    Failing (but correct) GirlScour test case

--- tests/broken_girlscout.py
+++ tests/broken_girlscout.py
@@ -1,38 +1,44 @@
-import sys
 import logging
-
 import nose
-
 import angr
-import simuvex
+import archinfo
+import os
+
+test_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries/tests'))
+l = logging.getLogger('angr.test_girlscout')
 
 def main():
-    if len(sys.argv) == 2:
-        f = sys.argv[1]
-    else:
-        f = "/home/angr/angr/rearentry/tests/dell-2e0048/dell-2e0048.out"
-    # TODO: Determine the architecture and endness
-    p = angr.Project(f, arch=simuvex.SimARM(endness="Iend_BE"),
+    file_path = "mipsel/fauxware"
+    f = os.path.join(test_location, file_path)
+    l.debug("Processing %s", f)
+
+    # "Correct" data by using the default loader
+    p_orig = angr.Project(f)
+
+    p = angr.Project(
+        f,
         load_options={
-            'backend': 'blob',
-            'custom_base_addr': 0x10000,
-            #'custom_entry_point': 0x10000,
-            'custom_entry_point': 0x10000,
-            'custom_arch': 'ARM',
-            'custom_offset': 0,
+            'main_opts': {
+                'backend': 'blob',
+                'custom_arch': p_orig.arch,
             }
-        )
-    # Call Scout
-    #p.analyses.Scout(start=0x16353c)
-    gs = p.analyses.GirlScout(pickle_intermediate_results=True)
-    nose.tools.assert_equal(gs.base_address, 0x40580000)
+        }
+    )
+    # Call Girlscout
+    gs = p.analyses.GirlScout(
+        pickle_intermediate_results=False,
+        perform_full_code_scan=False
+    )
+
+    # The base address detected by Girlscout should be equal to the original one
+    nose.tools.assert_equal(gs.base_address, p_orig.loader.main_bin.get_min_addr())
 
 if __name__ == "__main__":
     _debugging_modules = {
-        #'angr.analyses.girlscout'
-        }
-    _info_modules = {
         'angr.analyses.girlscout'
+    }
+    _info_modules = {
+        #'angr.analyses.girlscout'
     }
     _error_modules = {
         'angr.states'
